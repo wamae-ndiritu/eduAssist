c0ac6ee6d610d2aa82604af701ec708a322a7a97
Matuwamai <wamaimatu@gmail.com>
2024-04-04 14:57:49 +0300

    Style login page

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,18 +1,30 @@
 const LoginPage = () => {
   return (
-    <div className="bg-alice-blue w-1/5 item-align-center">
-      <h1>Welcome To EduAssist System</h1>
-      <h1 className="text-3xl font-bold  bg-red">Hello world!</h1>
-      <section className="">
-        <label className="">Email</label>
-        <input
-          type="email"
-          placeholder="wamaimatu@gmail.com"
-          className=""
-        ></input>
-        <label className="">Password</label>
-        <input type="password" placeholder="Aabcd123" className=""></input>
-      </section>
+    <div className="loginBody">
+      <container>
+        <h1 className="welcome">Welcome To EduAssist System</h1>
+        <section className="login">
+          <label className="py-1">Email</label>
+          <input
+            type="text"
+            placeholder="wamaimatu@gmail.com"
+            className="border px-4 py-2 rounded-lg text-gray-600 focus:outline-amber-400"
+          ></input>
+          <label className="py-1">Password</label>
+          <input
+            type="password"
+            placeholder="********"
+            className="border px-4 py-2 rounded-lg text-gray-600 focus:outline-amber-400"
+          ></input>
+          <button
+            className="bg-gray-900 border text-white rounded py-3 px-4 
+        text-xl font-semibold hover:bg-transparent hover:text-gray-900 hover:
+        border-gray-600"
+          >
+            Sign In
+          </button>
+        </section>
+      </container>
     </div>
   );
 };

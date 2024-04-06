654b65401f0c131f682f27c95d805d4a9f8e4fde
Matuwamai <wamaimatu@gmail.com>
2024-04-06 12:14:01 +0300

    style footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 const Footer = () => {
   return (
-    <div className="bg-emerald-300 text-white p-4">
-      <ul></ul>
+    <div className="bg-emerald-300 text-white p-8">
+      <ul>
+        <img
+          src="https://www.iconfinder.com/icons/5296521/play_video_vlog_youtube_youtube_logo_icon"
+          alt="youtube icon"
+        />
+      </ul>
     </div>
   );
 };

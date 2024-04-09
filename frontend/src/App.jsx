a7dd538afb62a4673223d17cb2f672fcf24ed89d
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-04-09 13:55:53 +0300

    Protect profile section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 import "./index.css";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
@@ -6,6 +12,16 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProfilePage from "./pages/ProfilePage";
+import { useSelector } from "react-redux";
+
+const ProtectedLayout = () => {
+  const { userInfo } = useSelector((state) => state.user);
+  if (userInfo?.token?.access) {
+    return <Outlet />;
+  }
+  return <Navigate to='/login' />;
+};
+
 function App() {
   return (
     <Router>
@@ -14,7 +30,9 @@ function App() {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/' element={<HomePage />} />
         <Route path='/about-us' element={<AboutPage />} />
-        <Route path='/profile' element={<ProfilePage />} />
+        <Route element={<ProtectedLayout />}>
+          <Route path='/profile' element={<ProfilePage />} />
+        </Route>
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </Router>
